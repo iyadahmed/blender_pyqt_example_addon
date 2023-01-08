b0810af4094d1ba1d365a67a5935ac88ce19dcc4
Iyad Ahmed <iyadahmed430@gmail.com>
2023-01-08 10:54:17 +0200

    Initial commit

---
+++ blender_pyqt6_example_addon/__init__.py
@@ -0,0 +1,49 @@
+bl_info = {
+    "name": "Cross Platform PyQt6 Addon Example",
+    "description": "",
+    "author": "Iyad Ahmed (iyadahmed430@gmail.com)",
+    "version": (0, 0, 1),
+    "blender": (3, 4, 1),
+    "location": "",
+    "warning": "",
+    "doc_url": "",
+    "tracker_url": "",
+    "support": "COMMUNITY",
+    "category": "Scripting",
+}
+
+import site
+from pathlib import Path
+
+# Add bundled packages directory to import search path
+# NOTE: this path has to match the directory where the bundled packages are installed to
+# the path has to be under the addon directory of course
+site.addsitedir((Path(__file__).parent / "extern").as_posix())
+
+from PyQt6.QtWidgets import QApplication, QLabel
+import bpy
+
+
+def event_loop_timer():
+    QApplication.processEvents()
+    return 0
+
+
+# Start a QApplication and loop timer if not started
+if QApplication.startingUp():
+    app = QApplication([])
+    bpy.app.timers.register(event_loop_timer)
+
+widgets = []
+
+
+def register():
+    label = QLabel("Hello from Qt: Addon was registered!")
+    label.show()
+    # We have to keep widgets stored somewhere,
+    # otherwise they will get garbage collected and destroyed
+    widgets.append(label)
+
+
+def unregister():
+    pass
---
+++ package.py
@@ -0,0 +1,32 @@
+import subprocess as sp
+from pathlib import Path
+from platform import platform, python_version
+from shutil import copytree, rmtree, make_archive
+
+PARENT_DIR = Path(__file__).parent
+OUT_DIR = PARENT_DIR / "out"
+
+# Delete existing "out" directory
+if OUT_DIR.exists():
+    if OUT_DIR.is_dir():
+        rmtree(PARENT_DIR / "out")
+    else:
+        raise RuntimeError("Output directory exists and it is not a directory, please remove it manually")
+
+# Copy source addon folder to a subdirectory of "out" directory,
+# we copy to a subdirectory because it makes it easier later to ZIP the addon,
+# and keep the addon directory in the ZIP (so it gets unzipped correctly by Blender)
+copytree(PARENT_DIR / "blender_pyqt6_example_addon", OUT_DIR / "package" / "blender_pyqt6_example_addon")
+
+# Install requirements to location expected by addon
+sp.run(["pip",
+        "install",
+        "-r",
+        PARENT_DIR / "requirements.txt",
+        "-t",
+        OUT_DIR / "package" / "blender_pyqt6_example_addon" / "extern"])
+
+# Create addon ZIP
+zip_filename = f"blender-pyqt6-example-addon-Python-{python_version()}-{platform()}"
+zip_filepath = (OUT_DIR / zip_filename).as_posix()
+make_archive(zip_filepath, "zip", root_dir=OUT_DIR / "package")
